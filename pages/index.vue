07d25e0bd81d752b8ae53dec8b62f417422a5be6
MmonimUkpanah <mukpanah22@gmail.com>
2021-01-20 14:48:23 +0100

    adde api

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,25 @@
 </template>
 
 <script>
-export default {};
+export default {
+    data(){
+        return{
+            stuff:{ }
+        }
+    },
+    mounted(){
+        this.getStuff()
+    },
+    methods:{
+        getStuff(){
+        this.$axios.get("https://api.jayceeandjay.com/rents", {headers : {'Content-Type':'application/json'}}).then((res)=> {
+               this.stuff=res.data;
+               console.log(this.stuff)
+         }) 
+    },
+}
+};
+
 </script>
 
 <style scoped>
